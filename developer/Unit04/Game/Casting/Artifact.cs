c6815fbb26e6db75508a53cfadf1a1508d9def78
Roberto Ramirez <rramirez@arkus-solutions.com>
2022-11-13 19:22:12 -0800

    Final details

--- developer/Unit04/Game/Casting/Artifact.cs
+++ developer/Unit04/Game/Casting/Artifact.cs
@@ -4,52 +4,43 @@ using System;
 
 namespace Unit04.Game.Casting
 {
-    // TODO: Implement the Artifact class here
-
-    // 1) Add the class declaration. Use the following class comment. Make sure you
-    //    inherit from the Actor class.
 
         /// <summary>
-        /// <para>An item of cultural or historical interest.</para>
+        /// <para>An item that inherits the Actor elements.</para>
         /// <para>
-        /// The responsibility of an Artifact is to provide a message about itself.
+        /// The responsibility of an Artifact is to provide a score about itself.
         /// </para>
         /// </summary>
         public class Artifact:Actor{
             private int score = 0;
 
-        
-
-
+    
     // 2) Create the class constructor. Use the following method comment.
         
         /// <summary>
         /// Constructs a new instance of Artifact.
         /// </summary>
             public Artifact(){
-
             }
        
-
-    // 3) Create the GetMessage() method. Use the following method comment.
+    // 3) Create the GetScore() method. Use the following method comment.
         
         /// <summary>
-        /// Gets the artifact's message.
+        /// Gets the artifact's score.
         /// </summary>
-        /// <returns>The message as a string.</returns>
+        /// <returns>The message as an integer.</returns>
             public int GetScore(){
                 
                 return score;
 
             }
-        
 
-    // 4) Create the SetMessage(string message) method. Use the following method comment.
+    // 4) Create the SetScore(int score) method. Use the following method comment.
         
         /// <summary>
-        /// Sets the artifact's message to the given value.
+        /// Sets the ScoreBoard score to the given value.
         /// </summary>
-        /// <param name="message">The given message.</param>
+        /// <param name="score">The given score.</param>
             public void SetScore(int score){
                 this.score = score;
 
