3d3aa131b88c7cd2f67e221574fb58263787037a
Roberto Ramirez <rramirez@arkus-solutions.com>
2022-11-20 17:20:38 -0800

    Implementation of MoveActorsAction.cs

--- articulate/Unit05/Game/Scripting/MoveActorsAction.cs
+++ articulate/Unit05/Game/Scripting/MoveActorsAction.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Unit05.Game.Casting;
+using Unit05.Game.Services;
 
 
 namespace Unit05.Game.Scripting
@@ -8,24 +9,38 @@ namespace Unit05.Game.Scripting
 
     // 1) Add the class declaration. Use the following class comment. Make sure you
     //    inherit from the Action class.
-
     /// <summary>
     /// <para>An update action that moves all the actors.</para>
     /// <para>
     /// The responsibility of MoveActorsAction is to move all the actors.
     /// </para>
     /// </summary>
+    public class MoveActorsAction : Action
 
-    // 2) Create the class constructor. Use the following method comment.
+// 2) Create the class constructor. Use the following method comment.
+    {
+        /// <summary>
+        /// Constructs a new instance of MoveActorsAction.
+        /// </summary>
+        public MoveActorsAction()
+        {            
+        }
 
-    /// <summary>
-    /// Constructs a new instance of MoveActorsAction.
-    /// </summary>
-
+        /// <inheritdoc/>
+   
     // 3) Override the Execute(Cast cast, Script script) method. Use the following 
     //    method comment. You custom implementation should do the following:
     //    a) get all the actors from the cast
     //    b) loop through all the actors
     //    c) call the MoveNext() method on each actor.
 
+        public void Execute(Cast cast, Script script)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            foreach (Actor actor in actors)
+            {
+                actor.MoveNext();
+            }
+        }
+    }
 }
