936324fbf896eeb65faa43af6e3afb306c745036
Roberto Ramirez <rramirez@arkus-solutions.com>
2022-12-07 00:17:58 -0800

    copy carpet

---
+++ developer/Unit06 copy/Practice2/Program copy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SandboxProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.SetFirstName("Roberto");
+            person1.SetLastName("Ramirez");
+
+            Console.WriteLine(person1.GetFullName());
+
+            ChurchMember person2 = new ChurchMember();
+            person2.SetFirstName("Veronica");
+            person2.SetLastName("Curiel");
+            person2.SetCalling("Primary President");
+
+            Console.WriteLine(person2.GetChurchMemberInfo());
+
+        }
+    }
+}
--- sandbox/SandboxProject/Program.cs
+++ sandbox/SandboxProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SandboxProject
 {
@@ -6,18 +8,69 @@ namespace SandboxProject
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person();
-            person1.SetFirstName("Roberto");
-            person1.SetLastName("Ramirez");
+            List<string> products = new List<string>();
+            Order order = new Order();
+            Product product = new Product();
+            product.productId(1);
+            product.name("shoes");
+            product.price(20);
+            product.quantityProduct(2);
+            product.PriceCount();
+            string result = product.detailProduct();
+            products.Add(result);
 
-            Console.WriteLine(person1.GetFullName());
+            product = new Product();
+            product.productId(2);
+            product.name("jacket");
+            product.price(50.10);
+            product.quantityProduct(5);
+            product.PriceCount();
+            result = product.detailProduct();
+            products.Add(result);
 
-            ChurchMember person2 = new ChurchMember();
-            person2.SetFirstName("Veronica");
-            person2.SetLastName("Curiel");
-            person2.SetCalling("Primary President");
+            Customer customer = new Customer();
+            customer.Name("Pedro");
+            customer.Street("Test");
+            customer.State("California");
+            customer.City("San diego");
+            customer.Country("Usa");
+            bool isUsa = customer.isUsa();
+            string name = customer.NameReturn();
+            string address = customer.FormaterAddress();
 
-            Console.WriteLine(person2.GetChurchMemberInfo());
+            order.CoutnOrder(name, isUsa, products, address);
+
+            products = new List<string>();
+            order = new Order();
+            product = new Product();
+            product.productId(1);
+            product.name("shoes");
+            product.price(20);
+            product.quantityProduct(2);
+            product.PriceCount();
+            result = product.detailProduct();
+            products.Add(result);
+
+            product = new Product();
+            product.productId(2);
+            product.name("jacket");
+            product.price(50.10);
+            product.quantityProduct(5);
+            product.PriceCount();
+            result = product.detailProduct();
+            products.Add(result);
+
+            customer = new Customer();
+            customer.Name("juan");
+            customer.Street("Test");
+            customer.State("Santa fe");
+            customer.City("chichuahua");
+            customer.Country("Mexico");
+            isUsa = customer.isUsa();
+            name = customer.NameReturn();
+            address = customer.FormaterAddress();
+
+            order.CoutnOrder(name, isUsa, products, address);
 
         }
     }
